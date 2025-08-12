a9fc650cbb6bfa37f538184826f514cdded38d55
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-08-12 20:24:51 +0900

    chore: 깃 초기 세팅

---
+++ Junction-404/Junction-404/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Junction-404
+//
+//  Created by Jay on 8/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Junction-404/Junction-404/Junction_404App.swift
@@ -0,0 +1,17 @@
+//
+//  Junction_404App.swift
+//  Junction-404
+//
+//  Created by Jay on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Junction_404App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
